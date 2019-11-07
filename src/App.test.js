d6929810350cbb32e119526360e022df9987063b
Ashik Liyakathali <ashik21395@gmail.com>
2019-11-07 11:34:29 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// src/App.test.js
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import renderer from "react-test-renderer";
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("App component", () => {
+  it("starts with a count of 0", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 0");
+  });
+  it("decrements count by 1 when the decrement button is clicked", () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find("button.decrement");
+    decrementBtn.simulate("click");
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: -1");
+  });
+});
+it("increments count by 1 when the increment button is clicked", () => {
+  const wrapper = shallow(<App />);
+  const incrementBtn = wrapper.find("button.increment");
+  incrementBtn.simulate("click");
+  const text = wrapper.find("p").text();
+  expect(text).toEqual("Count: 1");
+});
+it("matches the snapshot", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
